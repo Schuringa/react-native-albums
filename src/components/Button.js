93e0a21ee8cbc367e97a92012ee210a3f791fa03
jordans <schuringa.jordan@gmail.com>
2017-07-10 12:41:01 +1000

    Gave button component onpress prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
+import PropTypes from 'prop-types'
 
-const Button = () => {
+const Button = ({ onPress }) => {
   const { buttonStyle, textStyle } = styles
   return (
-    <TouchableOpacity
-      onPress={() => console.log('pressed')}
-      style={buttonStyle}
-    >
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
       <Text style={textStyle}>Click me!</Text>
     </TouchableOpacity>
   )
+}
+
+Button.propTypes = {
+  onPress: PropTypes.func
 }
 
 const styles = {
