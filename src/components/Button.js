46ee0e5c0829a703dc7d48f83df9980ab11545d7
jordans <schuringa.jordan@gmail.com>
2017-07-10 12:59:13 +1000

    Added button prop type for text

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,17 +2,18 @@ import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 import PropTypes from 'prop-types'
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles
   return (
     <TouchableOpacity onPress={onPress} style={buttonStyle}>
-      <Text style={textStyle}>Click me!</Text>
+      <Text style={textStyle}>{children}</Text>
     </TouchableOpacity>
   )
 }
 
 Button.propTypes = {
-  onPress: PropTypes.func
+  onPress: PropTypes.func,
+  children: PropTypes.object
 }
 
 const styles = {
