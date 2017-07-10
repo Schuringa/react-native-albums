e07fc2b7c1a1ebd4533a27762b4129fc37defed6
jordans <schuringa.jordan@gmail.com>
2017-07-10 11:55:13 +1000

    Added in images and image stylers. Deconsturucted props.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import PropTypes from 'prop-types'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album
+  const { thumbnailStyle, cardHeaderStyle } = styles
   return (
     <Card>
       <CardSection>
-        <View />
-        <View style={styles.cardHeaderStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={cardHeaderStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -26,6 +30,10 @@ const styles = {
   cardHeaderStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 }
 
