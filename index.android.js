ef35339b684395c3cc32bdb42116c0be7e285470
jordans <schuringa.jordan@gmail.com>
2017-07-10 10:26:26 +1000

    first Commit

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react'
+import { AppRegistry, Text } from 'react-native'
+
+// Creating component
+const App = () => <Text> Some test </Text>
+AppRegistry.registerComponent('albums', () => App)
---
+++ index.ios.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react'
+import { AppRegistry, View } from 'react-native'
+import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
+
+// Creating component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+)
+
+// Render to device
+AppRegistry.registerComponent('albums', () => App)
